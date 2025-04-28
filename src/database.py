1824a344dbd2ea184a08710dba0675ce568ccaa6
Ivan Fedorov <ivan45sc@gmail.com>
2025-04-28 10:26:33 +0500

    add connection to database

---
+++ src/database.py
@@ -0,0 +1,17 @@
+from typing import Annotated
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+
+from fastapi import Depends
+
+
+DATABASE_URL = "postgresql+asyncpg://birzha:birzha@localhost:5432/birzha"
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+new_async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+async def get_session():
+    async with new_async_session() as session:
+        yield session
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
