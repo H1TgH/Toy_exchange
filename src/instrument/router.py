f19515727b5699da298f9f71caf36e4500773981
Ivan <ivan45sc@gmail.com>
2025-05-02 09:14:04 +0500

    add protected endpoint to delete instrument

--- src/instrument/router.py
+++ src/instrument/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
 
 from src.database import SessionDep
@@ -9,6 +9,16 @@ from src.instrument.schemas import InstrumentCreateSchema
 
 
 instrument_router = APIRouter()
+
+@instrument_router.get('/api/v1/public/instrument', response_model=list[InstrumentCreateSchema])
+async def get_instruments_list(
+    session: SessionDep
+):
+    result = await session.execute(select(InstrumentModel))
+
+    instruments = result.scalars().all()
+
+    return instruments
 
 @instrument_router.post('/api/v1/admin/instrument', response_model=OkResponseSchema)
 async def create_instrument(
@@ -26,13 +36,21 @@ async def create_instrument(
 
     return {'success': True}
 
-@instrument_router.get('/api/v1/public/instrument', response_model=list[InstrumentCreateSchema])
-async def get_instruments_list(
-    session: SessionDep
+@instrument_router.delete('/api/v1/admin/instrument/{ticker}', response_model=OkResponseSchema)
+async def delete_instrument(
+    session: SessionDep,
+    ticker: str,
+    admin_user = Depends(get_current_admin)
 ):
-    result = await session.execute(select(InstrumentModel))
+    instrument = await session.scalar(select(InstrumentModel).where(InstrumentModel.ticker == ticker))
 
-    instruments = result.scalars().all()
-    print(instruments)
+    if not instrument:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Instrument not found"
+        )
 
-    return instruments
+    await session.delete(instrument)
+    await session.commit()
+
+    return {"success": True}
