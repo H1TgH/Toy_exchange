f31e2c27353e6585d562662f79d37ed5678d35e1
Ivan Fedorov <ivan45sc@gmail.com>
2025-06-06 21:55:41 +0500

    add dockerfile

--- src/database.py
+++ src/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import DeclarativeBase
 from fastapi import Depends
 
 
-DATABASE_URL = 'postgresql+asyncpg://birzha:birzha@localhost:5432/birzha'
+DATABASE_URL = 'postgresql+asyncpg://birzha:birzha@db:5432/birzha'
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 new_async_session = async_sessionmaker(engine, expire_on_commit=False)
