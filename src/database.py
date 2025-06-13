06f787d892b408ec4a6d7bd9523b31ac1c2d0244
Ivan Fedorov <ivan45sc@gmail.com>
2025-06-13 16:56:38 +0500

    decrease connection count to db

--- src/database.py
+++ src/database.py
@@ -22,12 +22,12 @@ engine = create_async_engine(
     DATABASE_URL,
     echo=True,
     poolclass=AsyncAdaptedQueuePool,
-    pool_size=40,  # Увеличено с 20 до 40
-    max_overflow=20,  # Увеличено с 10 до 20
-    pool_timeout=15,  # Уменьшено с 30 до 15
-    pool_recycle=1800,  # Пересоздание соединений каждые 30 минут
-    pool_pre_ping=True,  # Проверка соединений перед использованием
-    isolation_level='READ COMMITTED'  # Изменяем уровень изоляции
+    pool_size=10,
+    max_overflow=10,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True,
+    isolation_level='READ COMMITTED'
 )
 
 async_session = async_sessionmaker(
